da593f12b531ec467642eb334a51e94d96e4a877
FURU-RINA <131419121+FURU-RINA@users.noreply.github.com>
2023-11-03 08:31:01 +0000

    app6.jsの変更

--- create.js
+++ create.js
@@ -2,18 +2,18 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('test2.db');
 
 let schema = `
-create table test(
+create table maker(
   id integer primary key,
-  name text
+  name text not null
 );
 `
 
-db.serialize( () => {
-	db.run( schema, (error, row) => {
-		if(error) {
-			console.log('Error: ', error );
-			return;
-		}
-		console.log( "テーブルを作成しました" );
-	});
+db.serialize(() => {
+  db.run(schema, (error, row) => {
+    if (error) {
+      console.log('Error: ', error);
+      return;
+    }
+    console.log("テーブルを作成しました");
+  });
 });
