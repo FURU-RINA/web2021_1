5bc96f3718bccdb7176bcd3aa0731b267a73bd2c
FURU-RINA <131419121+FURU-RINA@users.noreply.github.com>
2024-01-10 20:06:07 +0000

    ummmm

--- AQselect.js
+++ AQselect.js
@@ -2,10 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('AQ.db');
 
 let sql = `
-  select SEA.name as name1, REVER.name as name2, MARINANIMAL.name as name3, SPETIAL.name as name4  from kind inner join SEA on SEA.id = kind.Se_id 
-inner join REVER on REVER.id = kind.Re_id 
-inner join MARINANIMAL on MARINANIMAL.id = kind.Ma_id 
-inner join SPETIAL on SPETIAL.id = kind.Sp_id;
+  select  SEA.name, REVER.name as name1 from ((kind outer join SEA on SEA.id=kind.Se_id) kind outer join REVER on REVER.id=kind.Re_id);
   `
 db.serialize( () => {
   db.all( sql, (error, row) => {
@@ -14,7 +11,7 @@ db.serialize( () => {
       return;
     }
     for( let data of row ) {
-     console.log( data.id + ' : ' + data.name1 + ' : '  + data.name2 + ' : ' + data.name3 + ' : ' + data.name4 );
+     console.log( data.name1 + ' : ' + data.name /*+ ' : '  + data.name2 + ' : ' + data.name3 + ' : ' + data.name4*/ );
     }
   });
 });
